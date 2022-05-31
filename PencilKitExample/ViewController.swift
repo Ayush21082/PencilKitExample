1ee85e22242ac7c7f753f7b476570a9f4157cf55
Ayush Singh <ayush.singh21082@gmail.com>
2022-05-31 22:59:11 +0530

    init

--- PencilKitExample/ViewController.swift
+++ PencilKitExample/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+    
+    private var canvasView: PKCanvasView = {
+        let canvas = PKCanvasView()
+        canvas.drawingPolicy = .anyInput
+        return canvas
+    }()
+    
+    let drawing = PKDrawing()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        canvasView.drawing = drawing
+        canvasView.delegate = self
+        view.addSubview(canvasView)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        canvasView.frame = view.bounds
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let toolPicker = PKToolPicker()
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
     }
 
+    //Canvas Delegates
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        
+    }
+    
+    func canvasViewDidFinishRendering(_ canvasView: PKCanvasView) {
+        
+    }
+    
+    
+    func canvasViewDidBeginUsingTool(_ canvasView: PKCanvasView) {
+        
+    }
+    
+    func canvasViewDidEndUsingTool(_ canvasView: PKCanvasView) {
+        
+    }
+    
 
+    
 }
 
